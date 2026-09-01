6c33fe48152ad6b0d9c8b8473bd0afc0507a7b12
tinodavin91-ctrl <tinodavin91@gmail.com>
2026-09-01 13:32:11 +0100

    Fix DatabaseSeeder filename casing for Linux compatibility

---
+++ database/seeders/databaseseeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // Add seed calls here later if needed, e.g.:
+        // $this->call([
+        //     UserSeeder::class,
+        //     VideoSeeder::class,
+        //     TrackSeeder::class,
+        //     ArticleSeeder::class,
+        // ]);
+    }
+}
